e6624f88f93d88b31241b41e9dfbd94ca7abf723
Omar Sotillo <omarsotillofranco@gmail.com>
2020-07-03 11:46:13 +0200

    refactor: structure
    Pages now contains page specific components and components shared ones.

--- pages/index/-confirm-request-modal.vue
+++ pages/index/-confirm-request-modal.vue
@@ -88,3 +88,8 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
---
+++ pages/index/-right-panel.vue
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+
+import PlatformOptions from './-options/-platform-options.vue'
+import AppOptions from './-options/-app-options.vue'
+import LayoutOptions from './-options/-layout-options.vue'
+import ExtraOptions from './-options/-extra-options.vue'
+
+export default Vue.extend({
+  name: 'right-panel',
+  template: require('./-right-panel.html'),
+  components: { PlatformOptions, AppOptions, LayoutOptions, ExtraOptions },
+})
