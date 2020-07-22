733d0de1154e62b54a47c6228aece7348615fdd3
Omar Sotillo <omarsotillofranco@gmail.com>
2020-07-22 10:37:24 +0200

    test: netlify

--- functions/nativeto.js
+++ functions/nativeto.js
@@ -4,35 +4,17 @@ var os = require('os')
 var fs = require('fs')
 
 exports.handler = function (event, context, callback) {
-  const options = JSON.parse(event.body)
-  nativefier({ ...options, ...{ out: os.tmpdir() } }, function (
-    error,
-    appPath
-  ) {
-    if (error) {
-      console.error(error)
-      return
-    }
-    const fileName = os.tmpdir() + options.appName + '.zip'
-    const fileOutput = fs.createWriteStream(fileName)
-    const archive = archiver('zip')
-
-    archive.pipe(fileOutput)
-    archive.directory(appPath, false)
-    archive.finalize()
-
-    fileOutput.on('close', function () {
-      fs.readFile(fileOutput.path, function read(err, data) {
+  fs.writeFile('/tmp/test.txt', 'testing', function (err) {
+    if (err) {
+      console.log(err)
+    } else {
+      fs.readFile('testing', function read(err, data) {
         callback(null, {
-          headers: {
-            'Content-Type': 'application/zip',
-            'Content-Disposition': 'attachment; filename=' + fileName,
-          },
           statusCode: 200,
-          body: data.toString('base64'),
+          body: JSON.stringify({}),
           isBase64Encoded: true,
         })
       })
-    })
+    }
   })
 }
