c112d489f1356a08771224f86ab978efd075634e
Lorenz Weiß <lorenz.weis@gmail.com>
2020-08-07 13:34:53 +0200

    - add try catch block for request and send it back to modal - show error in modal

--- pages/index/-confirm-request-modal.vue
+++ pages/index/-confirm-request-modal.vue
@@ -75,16 +75,11 @@
           </div>
         </div>
       </div>
-      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-        <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-          <button
-            type="button"
-            class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-yellow-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-yellow-500 focus:outline-none focus:border-yellow-700 focus:shadow-outline-yellow transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-            @click="$emit('convert')"
-          >
-            🚀
-          </button>
+      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row">
+        <span class="flex-1 w-full rounded-md shadow-sm sm:ml-3 sm:w-auto text-red-500">
+          <span v-if="error">Something went wrong 😢 Try again!</span>
         </span>
+
         <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
           <button
             type="button"
@@ -94,17 +89,27 @@
             Not now
           </button>
         </span>
+        <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+          <button
+            type="button"
+            class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-yellow-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-yellow-500 focus:outline-none focus:border-yellow-700 focus:shadow-outline-yellow transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+            @click="$emit('convert')"
+          >
+            🚀
+          </button>
+        </span>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-  props: {
-    modalShow: Boolean,
-    options: Object,
-  },
-})
+  import Vue from 'vue'
+  export default Vue.extend({
+    props: {
+      modalShow: Boolean,
+      options: Object,
+      error: Boolean,
+    },
+  })
 </script>
